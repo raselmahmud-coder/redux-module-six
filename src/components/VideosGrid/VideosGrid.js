5b2daf9c92f65838ba56063ed6652517660bbb88
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-08-31 00:43:24 +0800

    videos dispatch added

--- src/components/VideosGrid/VideosGrid.js
+++ src/components/VideosGrid/VideosGrid.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import VideoGridItem from './VideoGridItem';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideos } from "../../features/Videos/VideosSlice";
+import Spinner from "../Spinner/Spinner";
+import VideoGridItem from "./VideoGridItem";
 
 const VideosGrid = () => {
-    return (
-        <section className="pt-12">
-        <section className="pt-12">
-            <div
-                className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]"
-            >
-               <VideoGridItem/>
-               <VideoGridItem/>
+  const dispatch = useDispatch();
+  const { isError, error, isLoading, videos } = useSelector(
+    (state) => state.videos,
+  );
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, [dispatch]);
+  let content;
+  if (isLoading) content = <Spinner />;
 
-               {/* error section 
-                <div className="col-span-12">some error happened</div> */}
-            </div>
-        </section>
-    </section>    );
+  if (!isLoading && isError) content = <div className="col-span-12">{error}</div>;
+  if (!isError && !isLoading && videos?.length === 0)
+    content = <div className="col-span-12"> There is no videos</div>;
+  if (!isError && !isLoading && videos?.length > 0)
+    content = videos.map((video) => (
+      <VideoGridItem key={video.id} video={video} />
+    ));
+
+  return (
+    <section className="pt-12">
+      <section className="pt-12">
+        <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
+          {content}
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default VideosGrid;
